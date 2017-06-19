e68b5b7ccddd2414aa4e2f1915a6e6919662481a
Sergey Ukustov <sergey@ukstv.me>
2017-06-19 20:57:36 +0300

    Declare Dnode

---
+++ types/dnode/index.d.ts
@@ -0,0 +1,98 @@
+declare module "dnode/browser" {
+  import {Readable, Writable} from "stream";
+
+  class Dnode implements Writable {
+    pipe<T extends Writable>(destination: T, options?: { end?: boolean }): T;
+
+    removeAllListeners(event?: string | symbol): this;
+    removeAllListeners(event?: string | symbol): this;
+
+    setMaxListeners(n: number): this;
+    setMaxListeners(n: number): this;
+
+    getMaxListeners(): number;
+
+    listeners(event: string | symbol): Function[];
+
+    eventNames(): (string | symbol)[];
+
+    listenerCount(type: string | symbol): number;
+    writable: boolean;
+
+    _write(chunk: any, encoding: string, callback: Function): void;
+
+    write(chunk: any, cb?: Function): boolean;
+    write(chunk: any, encoding?: string, cb?: Function): boolean;
+    write(buffer: Buffer | string, cb?: Function): boolean;
+    write(str: string, encoding?: string, cb?: Function): boolean;
+
+    setDefaultEncoding(encoding: string): this;
+
+    end(): void;
+    end(chunk: any, cb?: Function): void;
+    end(chunk: any, encoding?: string, cb?: Function): void;
+    end(buffer: Buffer, cb?: Function): void;
+    end(str: string, cb?: Function): void;
+    end(str: string, encoding?: string, cb?: Function): void;
+
+    addListener(event: string, listener: Function): this;
+    addListener(event: "close", listener: () => void): this;
+    addListener(event: "drain", listener: () => void): this;
+    addListener(event: "error", listener: (err: Error) => void): this;
+    addListener(event: "finish", listener: () => void): this;
+    addListener(event: "pipe", listener: (src: Readable) => void): this;
+    addListener(event: "unpipe", listener: (src: Readable) => void): this;
+
+    emit(event: string, ...args: any[]): boolean;
+    emit(event: "close"): boolean;
+    emit(event: "drain", chunk: Buffer | string): boolean;
+    emit(event: "error", err: Error): boolean;
+    emit(event: "finish"): boolean;
+    emit(event: "pipe", src: Readable): boolean;
+    emit(event: "unpipe", src: Readable): boolean;
+
+    on(event: string, listener: Function): this;
+    on(event: "close", listener: () => void): this;
+    on(event: "drain", listener: () => void): this;
+    on(event: "error", listener: (err: Error) => void): this;
+    on(event: "finish", listener: () => void): this;
+    on(event: "pipe", listener: (src: Readable) => void): this;
+    on(event: "unpipe", listener: (src: Readable) => void): this;
+
+    once(event: string, listener: Function): this;
+    once(event: "close", listener: () => void): this;
+    once(event: "drain", listener: () => void): this;
+    once(event: "error", listener: (err: Error) => void): this;
+    once(event: "finish", listener: () => void): this;
+    once(event: "pipe", listener: (src: Readable) => void): this;
+    once(event: "unpipe", listener: (src: Readable) => void): this;
+
+    prependListener(event: string, listener: Function): this;
+    prependListener(event: "close", listener: () => void): this;
+    prependListener(event: "drain", listener: () => void): this;
+    prependListener(event: "error", listener: (err: Error) => void): this;
+    prependListener(event: "finish", listener: () => void): this;
+    prependListener(event: "pipe", listener: (src: Readable) => void): this;
+    prependListener(event: "unpipe", listener: (src: Readable) => void): this;
+
+    prependOnceListener(event: string, listener: Function): this;
+    prependOnceListener(event: "close", listener: () => void): this;
+    prependOnceListener(event: "drain", listener: () => void): this;
+    prependOnceListener(event: "error", listener: (err: Error) => void): this;
+    prependOnceListener(event: "finish", listener: () => void): this;
+    prependOnceListener(event: "pipe", listener: (src: Readable) => void): this;
+    prependOnceListener(event: "unpipe", listener: (src: Readable) => void): this;
+
+    removeListener(event: string, listener: Function): this;
+    removeListener(event: "close", listener: () => void): this;
+    removeListener(event: "drain", listener: () => void): this;
+    removeListener(event: "error", listener: (err: Error) => void): this;
+    removeListener(event: "finish", listener: () => void): this;
+    removeListener(event: "pipe", listener: (src: Readable) => void): this;
+    removeListener(event: "unpipe", listener: (src: Readable) => void): this;
+  }
+
+  function DnodeConstructor(cons?: Object): Dnode;
+
+  export default DnodeConstructor;
+}
