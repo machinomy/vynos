df9c36b97020e6b9130c395f632cf0a86a2350ef
xjenek <xxjenekxx@gmail.com>
2017-11-02 19:59:06 +0300

    Fake Network, Preferences tabs

--- vynos/frame/components/Account/Network/index.tsx
+++ vynos/frame/components/Account/Network/index.tsx
@@ -1,32 +1,47 @@
 import * as React from "react";
-import { Container, Form, Radio, Button } from 'semantic-ui-react'
+import { Container, Form, Radio } from 'semantic-ui-react'
 const style = require("../../../styles/ynos.css");
 
 
-export interface NetworkProps { }
+export interface NetworkProps {
+}
 
-//const Network: React.SFC<NetworkProps> = (props) => {
-export class Network extends React.Component<any, any> {
+export interface NetworkState {
+  value: string
+}
 
+export class Network extends React.Component<NetworkProps, NetworkState> {
 
-    render() {
+  networks: [string];
 
-        return (
-            <Container className={style.clearBorder}>
-                <Form className={style.formNetwork}>
-                    <Form.Group grouped>
-                        <Form.Field control={Radio} label='One' value='1' />
-                    </Form.Group>
-                    <Form.Group grouped>
-                        <Form.Field control={Radio} label='Two' value='2'  />
-                    </Form.Group>
-                    <Form.Group grouped>
-                        <Form.Field control={Radio} label='Three' value='3'  />
-                    </Form.Group>
-                </Form>
-            </Container>
-        )
-    }
+  constructor () {
+    super();
+  }
+
+  componentDidMount(){
+    this.networks = ['Ropsten', 'Rinkeby', 'Main'];
+    this.setState({value: 'Ropsten'});
+  }
+
+  setRadio (ev: Event, input: HTMLInputElement) {
+    this.setState({value: input.value})
+  }
+
+  render () {
+    return (
+      <Container className={style.clearBorder}>
+        <Form className={style.formNetwork}>
+          <Form.Group grouped>
+            {this.networks && this.networks.map((network) => {
+              return <Form.Field key={network}><Radio name={'network'} label={network} value={network}
+                                        onChange={this.setRadio.bind(this)}
+                                        checked={this.state.value === network} style={{width: '100%'}} disabled/></Form.Field>
+            })}
+          </Form.Group>
+        </Form>
+      </Container>
+    )
+  }
 };
 
 export default Network;
