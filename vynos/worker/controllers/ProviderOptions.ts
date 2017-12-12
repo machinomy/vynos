c395ae40f76ec1180fecc690dc0193f43b367832
Alexandr Burtovoy <volgobur@gmail.com>
2017-12-12 19:51:34 +0300

    Fix reject message

--- vynos/worker/controllers/ProviderOptions.ts
+++ vynos/worker/controllers/ProviderOptions.ts
@@ -32,9 +32,13 @@ export default class ProviderOptions {
   approveTransaction(txParams: any, callback: ApproveTransactionCallback) {
     let transaction = transactions.ethereum(randomId().toString(), txParams.to, txParams.value, 0)
     this.transactions.approveTransaction(transaction).then(result => {
-      callback(null, result)
+      if (result) {
+        callback(null, result)
+      } else {
+        callback('Vynos: User rejected transaction')
+      }
     }).catch(error => {
-      callback(error)
+      callback(error.message)
     })
   }
 
