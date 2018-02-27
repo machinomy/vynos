0afa2b5c1f6a0d9d79bcb6af8e55fb388023811b
xjenek <xxjenekxx@gmail.com>
2018-02-27 14:05:28 +0300

    channelId: ChannelId

--- vynos/lib/VynosBuyResponse.ts
+++ vynos/lib/VynosBuyResponse.ts
@@ -2,6 +2,6 @@ import {ChannelId, PaymentChannel} from "machinomy/lib/channel";
 import Payment from "machinomy/lib/Payment";
 
 export default interface VynosBuyResponse {
-  channelId: string
+  channelId: ChannelId
   token: string
 }
