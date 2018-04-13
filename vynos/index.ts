90400dd45abaef0b009be1f83b31c631511f419e
Andrei Riaskov <ryaskov.work@gmail.com>
2018-04-13 19:47:35 +0300

    Fix re-export of types.

--- vynos/index.ts
+++ vynos/index.ts
@@ -8,7 +8,7 @@ let wallet = new Wallet(setup.client(), setup.frame())
 export { WalletBuyArguments } from './lib/Vynos'
 export { BuyProcessEvent } from './lib/rpc/buyProcessEventBroadcast'
 export { ChannelMeta } from './lib/storage/ChannelMetaStorage'
-export * from './lib/promised'
-export * from './lib/VynosBuyResponse'
+export { default as PromisedWalletResponse } from './lib/promised'
+export { default as VynosBuyResponse } from './lib/VynosBuyResponse'
 
 export default wallet
