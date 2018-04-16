d372dad6f0a8af2c0a9880433293371c0d1a37c4
Andrei Riaskov <ryaskov.work@gmail.com>
2018-04-16 19:34:06 +0300

    Remove fixer-api, crypto-price. Firefox support.

--- vynos/frame/lib/currency.ts
+++ vynos/frame/lib/currency.ts
@@ -1,10 +1,9 @@
 import BigNumber = require('bignumber.js')
-const price = require('crypto-price')
-const fixer = require('fixer-api')
 
 export default class Currency {
   private static _instance: Currency
 
+  currencies: Map<string, string>
   _cacheTime: number
   _toCacheArray: Map<string, number>
   _fromCacheArray: Map<string, number>
@@ -12,6 +11,18 @@ export default class Currency {
   private _timer: any
 
   constructor (cacheTime?: number) {
+    this.currencies = new Map<string, string>()
+    this.currencies.set('ETH', 'Ethereum')
+    this.currencies.set('BTC', 'Bitcoin')
+    this.currencies.set('LTC', 'Litecoin')
+    this.currencies.set('AUD', 'Australian Dollar')
+    this.currencies.set('BRL', 'Brazilian Real')
+    this.currencies.set('CAD', 'Canadian Dollar')
+    this.currencies.set('GBP', 'British Pound')
+    this.currencies.set('JPY', 'Japanese Yen')
+    this.currencies.set('PLN', 'Polish Zloty')
+    this.currencies.set('RUB', 'Russian Ruble')
+    this.currencies.set('USD', 'US Dollar')
     let cacheTimeInMs: number = 30000
     if (cacheTime !== undefined) {
       if (cacheTime < 1) {
@@ -36,11 +47,11 @@ export default class Currency {
   async runInterval (interval: number) {
     this._timer = setInterval(async () => {
       this._toCacheArray.forEach(async (_, key) => {
-        this._toCacheArray.set(key, await this.getFixerRate(key))
+        this._toCacheArray.set(key, await this.fetchRate('USD', key))
       })
 
       this._fromCacheArray.forEach(async (_, key) => {
-        this._fromCacheArray.set(key, await this.getCryptoPriceRate(key))
+        this._fromCacheArray.set(key, await this.fetchRate(key, 'USD'))
       })
     }, interval)
 
@@ -50,10 +61,10 @@ export default class Currency {
   async convertCryptoOrCurrencyToCurrency (amount: BigNumber.BigNumber | number, fromCurrencyCode: string, toCurrencyCode: string): Promise<BigNumber.BigNumber> {
     let result: BigNumber.BigNumber = new BigNumber.BigNumber(0)
     if (!this._fromCacheArray.has(fromCurrencyCode)) {
-      this._fromCacheArray.set(fromCurrencyCode, await this.getCryptoPriceRate(fromCurrencyCode))
+      this._fromCacheArray.set(fromCurrencyCode, await this.fetchRate(fromCurrencyCode, 'USD'))
     }
     if (!this._toCacheArray.has(toCurrencyCode)) {
-      this._toCacheArray.set(toCurrencyCode, await this.getFixerRate(toCurrencyCode))
+      this._toCacheArray.set(toCurrencyCode, await this.fetchRate('USD', toCurrencyCode))
     }
     try {
       let bigNumberAmount = new BigNumber.BigNumber(amount)
@@ -69,27 +80,18 @@ export default class Currency {
     return new Promise<BigNumber.BigNumber>(resolve => {resolve(result)})
   }
 
-  async getFixerRate (toCurrencyCode: string): Promise<number> {
-    let result: number
-    try {
-      if (toCurrencyCode === 'USD') {
-        result = 1.0
-      } else {
-        result = (await fixer.latest({ base: 'USD', symbols: [toCurrencyCode] })).rates[toCurrencyCode]
-      }
-    } catch (e) {
-      console.error(e)
-    }
-    return new Promise<number>(resolve => {resolve(result)})
+  fetchRate (fromCurrency: string, toCurrency: string): Promise<number> {
+    const api = 'https://min-api.cryptocompare.com/data/price' + '?fsym=' + fromCurrency + '&tsyms=' + toCurrency
+    return new Promise<number>((done, err) => {
+      fetch(api).then(response => {
+        if (response.status === 200) {
+          return response.json()
+        } else {
+          err(`Error while getting rate. Response status: ` + response.status)
+        }
+      }).then(json => done(json[toCurrency]))
+        .catch(error => console.error(`Error while getting rate. Error: ` + error))
+    })
   }
 
-  async getCryptoPriceRate (fromCurrencyCode: string): Promise<number> {
-    let result: number
-    try {
-      result = parseFloat((await price.getBasePrice(fromCurrencyCode, 'USD')).price)
-    } catch (e) {
-      console.error(e)
-    }
-    return new Promise<number>(resolve => {resolve(result)})
-  }
 }
