9f83de6b8492862be294bc22e855d2bdd5f4f602
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-05-14 17:44:00 +0300

    Set global vynos object

--- vynos/index.ts
+++ vynos/index.ts
@@ -1,9 +1,15 @@
 import Wallet from './embed/Wallet'
 import Setup from './embed/Setup'
 import * as resourceAddress from './lib/resourceAddress'
+import IWalletWindow from './lib/IWalletWindow'
 
 let setup = new Setup(resourceAddress.embed(), window)
 let wallet = new Wallet(setup.client(), setup.frame())
+
+let w = window as IWalletWindow
+if (w && !w.vynos) {
+  w.vynos = wallet
+}
 
 export { WalletBuyArguments } from './lib/Vynos'
 export { BuyProcessEvent } from './lib/rpc/buyProcessEventBroadcast'
